80c1770f52d7adf6306c7d08d12f6ebba34ebe7a
meena2003 <meena2003@naver.com>
2022-10-28 22:59:23 +0900

    Calendar 클래스에 '윤년' 체크하기 기능 추가.

--- src/codeSquad/JavaCalendar/Calendar.java
+++ src/codeSquad/JavaCalendar/Calendar.java
@@ -3,27 +3,27 @@ package codeSquad.JavaCalendar;
 public class Calendar {
 
 	private static final int[] MAX_DAYS = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	private static final int[] LEAP_MAX_DAYS = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
-	public int getMaxDaysOfMonth(int month) {
-		return MAX_DAYS[month - 1];
+	public boolean isLeapYear(int year) {
+		if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 
-	public void printSampleCalendar() {
-		System.out.println("월  화  수  목  금  토  일");
-		System.out.println("----------------------");
-		System.out.println(" 1  2  3  4   5  6  7");
-		System.out.println(" 8  9 10 11  12 13 14");
-		System.out.println("15 16 17 18  19 20 21");
-		System.out.println("22 23 24 25  26 27 28");
+	public int getMaxDaysOfMonth(int year, int month) {
+		return isLeapYear(year) ? LEAP_MAX_DAYS[month - 1] : MAX_DAYS[month - 1];
 	}
-	
+
 	public void printCalendar(int year, int month) {
 		System.out.printf("    <<%4d년%3d월>>\n", year, month);
 		System.out.println(" SU MO TU WE TH FR SA");
 		System.out.println("----------------------");
-		
-		int maxDay = getMaxDaysOfMonth(month);
-		
+
+		int maxDay = getMaxDaysOfMonth(year, month);
+
 		for (int i = 1; i <= maxDay; i++) {
 			System.out.printf("%3d", i);
 			if (i % 7 == 0) {
@@ -31,7 +31,7 @@ public class Calendar {
 			}
 		}
 		System.out.println();
-		
+
 //		System.out.println(" 1  2  3  4  5  6  7");
 //		System.out.println(" 8  9 10 11 12 13 14");
 //		System.out.println("15 16 17 18 19 20 21");
--- src/codeSquad/JavaCalendar/Prompt.java
+++ src/codeSquad/JavaCalendar/Prompt.java
@@ -4,23 +4,24 @@ import java.util.Scanner;
 
 public class Prompt {
 	
-	private final static String PROMPT = "cal> ";
-	
 	public void runPrompt() {
 		Scanner scan = new Scanner(System.in);
 		Calendar cal = new Calendar();
 
 		while (true) {
+			System.out.println("년도을 입력하세요.");
+			System.out.print("YEAR> ");
+			int year = scan.nextInt();
 			System.out.println("월을 입력하세요.");
-			System.out.print(PROMPT);
+			System.out.print("MONTH> ");
 			int month = scan.nextInt();
-			if (month == -1) {
+			if (year == -1 || month == -1) {
 				break;
 			} else if (month == 0 || month > 12 || month < -2) {
 				System.out.println("올바른 범위가 아닙니다!");
 				continue;
 			}
-			cal.printCalendar(2022, month);
+			cal.printCalendar(year, month);
 		}
 		System.out.println("Have a nice day~");
 		scan.close();
