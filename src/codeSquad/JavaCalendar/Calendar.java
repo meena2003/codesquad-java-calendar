ca6bceb938a7cabdfbb8987fcf968229ee7e2392
Luka <meena2003@naver.com>
2022-10-29 22:04:40 +0900

    Promp에 exit 기능 소개 추가

--- src/codeSquad/JavaCalendar/Calendar.java
+++ src/codeSquad/JavaCalendar/Calendar.java
@@ -24,16 +24,19 @@ public class Calendar {
 
 		int maxDay = getMaxDaysOfMonth(year, month);
 		
+		// print blank space
 		for (int i = 0; i < weekDay; i++) {
 			System.out.printf("%3s", " ");
 		}
 		
+		// print from first line to last
 		for (int i = 1; i <= maxDay; i++) {
 			System.out.printf("%3d", i);
 			if ((i + weekDay) % 7 == 0) {
 				System.out.println();
 			}
 		}
+		System.out.println();
 		System.out.println();
 	}
 }
--- src/codeSquad/JavaCalendar/Prompt.java
+++ src/codeSquad/JavaCalendar/Prompt.java
@@ -2,63 +2,69 @@ package codeSquad.JavaCalendar;
 
 import java.util.Scanner;
 
-public class Prompt {	
+public class Prompt {
 	public void runPrompt() {
 		Scanner scan = new Scanner(System.in);
 		Calendar cal = new Calendar();
 
 		while (true) {
-			System.out.println("년도을 입력하세요.");
+			System.out.println("년도을 입력하세요.(exit: -1)");
 			System.out.print("YEAR> ");
 			int year = scan.nextInt();
-			System.out.println("월을 입력하세요.");
+			if (year == -1) {
+				break;
+			}
+
+			System.out.println("월을 입력하세요.(exit: -1)");
 			System.out.print("MONTH> ");
 			int month = scan.nextInt();
-			System.out.print("첫번째 요일을 입력하세요. (SU MO TU WE TH FR SA)\n");
-			System.out.print("WEEKDAY> ");
-			int weekDay = parseDay(scan.next());
-			
-			if (year == -1 || month == -1) {
+			if (month == -1) {
 				break;
 			} else if (month == 0 || month > 12 || month < -2) {
 				System.out.println("올바른 범위가 아닙니다!");
 				continue;
 			}
+
+			System.out.print("첫번째 요일을 입력하세요.(SU MO TU WE TH FR SA)\n");
+			System.out.print("WEEKDAY> ");
+			int weekDay = parseDay(scan.next().toUpperCase());
+
 			cal.printCalendar(year, month, weekDay);
 		}
+
 		System.out.println("Have a nice day~");
 		scan.close();
 	}
-	
+
 	public int parseDay(String week) {
 		int blank = 0;
 		switch (week) {
-			case "SU":
-				blank = 0;
-				break;
-			case "MO":
-				blank = 1;
-				break;
-			case "TU":
-				blank = 2;
-				break;
-			case "WE":
-				blank = 3;
-				break;
-			case "TH":
-				blank = 4;
-				break;
-			case "FR":
-				blank = 5;
-				break;
-			case "SA":
-				blank = 6;
-				break;
+		case "SU":
+			blank = 0;
+			break;
+		case "MO":
+			blank = 1;
+			break;
+		case "TU":
+			blank = 2;
+			break;
+		case "WE":
+			blank = 3;
+			break;
+		case "TH":
+			blank = 4;
+			break;
+		case "FR":
+			blank = 5;
+			break;
+		case "SA":
+			blank = 6;
+			break;
 		}
-		
+
 		return blank;
 	}
-	
+
 	public static void main(String[] args) {
 		Prompt p = new Prompt();
 		p.runPrompt();
